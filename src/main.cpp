f312b2c96f675628b703ae2bcf3dd7206c3dc2d1
Kyle Timmermans <47165102+kyletimmermans@users.noreply.github.com>
2021-05-01 10:08:56 -0400

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "shark.h"
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	// Print logo
+	shark logo;
+	logo.printShark();
+	return 0;
+}
---
+++ src/shark/shark.h
@@ -0,0 +1,7 @@
+#include <iostream>
+
+class shark
+{
+	public:
+		 void printShark();
+};
